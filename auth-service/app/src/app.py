aca5f3c0ae5eda82f7730bb485c4b80baac803a1
Sameer <sameer.barha12@gmail.com>
2024-09-09 14:29:04 +0530

    working sample endpoints

--- auth-service/app/src/app.py
+++ auth-service/app/src/app.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
 import logging
 
 
+from src.config import get_settings,Settings
 log = logging.getLogger("uvicorn")
 from contextlib import asynccontextmanager
 
@@ -12,3 +13,13 @@ async def lifespan(app: FastAPI):
     log.info("Shutting down...")
 
 app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/ping")
+async def pong(settings: Settings = Depends(get_settings)):
+    #return { "ping": settings }
+    return "pong"
+
+@app.get("/")
+async def welcome():
+    return { "service": "auth" }
