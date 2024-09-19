c7b8688a4d98ba225473b1fd5a1cea9da6897932
Sameer <sameer.barha12@gmail.com>
2024-09-19 16:15:02 +0530

    updated env variables and config

--- product-service/src/config/config.ts
+++ product-service/src/config/config.ts
@@ -5,6 +5,10 @@ dotenv.config();
 export const config = {
   mongodb: {
     uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/your_database',
+    db: process.env.MONGO_DB || 'apparel-galaxy',
+    inventory_collection: process.env.MONGO_INVENTORY_COLLECTION || 'inventory',
+    user_collection: process.env.MONGO_USER_COLLECTION || 'users',
+    cart_collection: process.env.MONGO_CART_COLLECTION || 'cart',
   },
   port: process.env.PORT || 3000,
   AUTH_SERVICE: process.env.AUTH_SERVICE || "http://localhost:8000",
@@ -12,4 +16,5 @@ export const config = {
   JWT_SECRET: process.env.JWT_SECRET || "your_secret_key",
   JWT_ALGORITHM: process.env.JWT_ALGORITHM || "HS256",
   JWT_EXPIRATION_TIME_MINUTES: process.env.JWT_EXPIRATION_TIME_MINUTES || 10,
+  
 };
