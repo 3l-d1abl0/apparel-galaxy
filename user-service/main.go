5f0a0a2fed22ef2047a418ebc9d0335b547c7388
Sameer <sameer.barha12@gmail.com>
2024-09-23 12:10:46 +0530

    added logger

--- user-service/main.go
+++ user-service/main.go
@@ -2,13 +2,36 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
+
+var logFile *os.File
+var logger = logrus.New()
 
 func init() {
 	fmt.Println("INit ...")
+
+	//debug mode for development
+	gin.SetMode(os.Getenv("GIN_MODE"))
+
+	//Set up logging
+	logFile, err := os.OpenFile("logs/user-service.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		logger.Fatal("Error handling log File: ", err)
+	}
+
+	//Set up destination
+	fileNStdout := io.MultiWriter(os.Stdout, logFile)
+	logger.SetOutput(fileNStdout)
+
+	//Set up format
+	logger.SetFormatter(&logrus.JSONFormatter{})
+
 }
 
 func main() {
@@ -16,5 +39,9 @@ func main() {
 	//Gin setup
 	app := gin.Default()
 
-	log.Fatal(app.Run(":8090"))
+	port := os.Getenv("PORT")
+
+	logger.Info("Starting Server on: ", port)
+	log.Fatal(app.Run(fmt.Sprintf(":%s", port)))
+
 }
