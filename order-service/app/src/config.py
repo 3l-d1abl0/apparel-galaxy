278ed1c30226882b65ce45a8e6d0be6a44bc3027
Sameer <sameer.barha12@gmail.com>
2024-09-24 16:21:32 +0530

    updated mongoDB URI

--- order-service/app/src/config.py
+++ order-service/app/src/config.py
@@ -9,10 +9,11 @@ class Settings(BaseSettings):
 
     environment: str = os.getenv("environment", "dev")
     testing: bool = os.getenv("testing", False)
-    MONGO_URI: str = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+    MONGODB_URI: str = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
     MONGO_DB: str = os.getenv("MONGO_DB", "apparel-galaxy")
     MONGO_INVENTORY_COLLECTION:str = os.getenv("MONGO_INVENTORY_COLLECTION", "inventory")
     MONGO_USER_COLLECTION: str = os.getenv("MONGO_USER_COLLECTION", "users")
+    MONGO_ORDERS_COLLECTION: str = os.getenv("MONGO_USER_COLLECTION", "orders")
     JWT_SECRET: str = os.getenv("JWT_SECRET", "your_secret_key")
     JWT_ALGORITHM: str = os.getenv("JWT_ALGORITHM", "HS256")
     JWT_EXPIRATION_TIME_MINUTES: int = int(os.getenv("JWT_EXPIRATION_TIME_MINUTES", 5))
